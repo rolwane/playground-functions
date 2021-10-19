b0bd221441eb4fbf1d927d6dea1c2f7864867a95
Rolwane Borges <rolwane@hotmail.com>
2021-10-19 15:08:07 -0300

    Requisito 10 - Crie uma função de Lista de tecnologias

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,33 @@
-// Desafio 10
-function techList() {
-  // seu código aqui
+// Função que compara os nomes das tecnologias para usar de parâmetro no método sort();
+// Usei como referência esse site: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+// Para saber obter mais detalhes sobre o método sort
+
+function comparaTech(a, b) {
+  if (a.tech > b.tech) {
+    return 1;
+  }
+  if (a.tech < b.tech) {
+    return -1;
+  }
+  return 0;
 }
+
+// Desafio 10
+function techList(techs, nome) {
+  if (techs[0] !== undefined) {
+    let arrTech = [];
+    for (let i = 0; i < techs.length; i += 1) {
+      arrTech.push({
+        tech: techs[i],
+        name: nome,
+      });
+    }
+    return arrTech.sort(comparaTech);
+  }
+  return 'Vazio!';
+}
+
+techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Rolwane');
 
 // Desafio 11
 function generatePhoneNumber() {
